207085e6ce1a962d8715091320b88e8ac6506e20
charlesjansen <vondorpp@gmail.com>
2017-06-11 19:32:37 -0300

    added regression problem on whole image, before editing

--- experiment-charles/slicing/slicingPict.py
+++ experiment-charles/slicing/slicingPict.py
@@ -1,2 +1,37 @@
+#train
+#format 1  5616 * 3744 
+#format 2  4992 * 3328
+#2 exception 3744 * 5616  (530 & 638)
+
+#test
+#format   5760 * 3840  
+#format   5616 * 3744
+#format   5184 * 3456
+#format   4608 * 3456
+#1 exception in test: 881 * 1280
+
+im = Image.open("F:/DS-main/Kaggle-main/NOAA Fisheries Steller Sea Lion Population Count - inputs/slicetest/0_13_07.png")
+im =im.resize(size)
+im.save("F:/DS-main/Kaggle-main/NOAA Fisheries Steller Sea Lion Population Count - inputs/slicetest/0_13_07.jpeg", "JPEG")
+
+
+
+
+
 import image_slicer
 image_slicer.slice('F:/DS-main/Kaggle-main/NOAA Fisheries Steller Sea Lion Population Count - inputs/slicetest/0.jpg', 225)
+
+import os, sys
+from PIL import Image
+
+size = 244, 224
+
+
+
+
+im = Image.open("F:/DS-main/Kaggle-main/NOAA Fisheries Steller Sea Lion Population Count - inputs/slicetest/0_13_07.png")
+im =im.resize(size)
+im.save("F:/DS-main/Kaggle-main/NOAA Fisheries Steller Sea Lion Population Count - inputs/slicetest/0_13_07.jpeg", "JPEG")
+
+
+
